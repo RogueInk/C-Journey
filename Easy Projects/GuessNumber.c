97ba4203f362a6ed54533285081e98147dc45f8b
Rogueink <animrogues@gmail.com>
2026-07-28 16:09:59 +0530

    Project 2 , Basic yet little complicated

--- Easy Projects/GuessNumber.c
+++ Easy Projects/GuessNumber.c
@@ -1,1 +1,35 @@
-// WIP
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    int guess = 0;
+    int attempts = 0;
+    int min = 1;
+    int max = 100;
+    
+    srand(time(NULL)); // Seed the random number generator
+
+    int answer = (rand() % (max - min + 1)) + min; // Generate a random number between min and max
+    do
+    {
+        printf("***WELCOME TO THE GUESSING GAME***\n");
+        printf("Guess a number between %d - %d: ", min, max);
+        scanf("%d", &guess);
+        attempts++;
+        if (guess < answer){
+            printf("Too low! Try again.\n");
+        }
+        else if (guess > answer){
+            printf("Too high! Try again.\n");
+        }
+        else{
+            printf("Correct!\n");
+        }
+    } while(guess != answer);
+     printf("Congratulations! You guessed the number %d in %d attempts.\n", answer, attempts);
+
+    return 0;
+    
+}
